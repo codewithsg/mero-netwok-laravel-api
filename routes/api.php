fe69472cef5c96ce355859205b67205045904c2b
saragam ghimire <codewithghimire@gmail.com>
2022-11-23 10:32:13 +0545

    short change in route

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('auth/register', [UserController::class, 'store']);
 Route::post('auth/login', [UserController::class, 'login']);
 Route::post('products/add', [ProductController::class, 'store']);
-Route::get('/products/getall', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
